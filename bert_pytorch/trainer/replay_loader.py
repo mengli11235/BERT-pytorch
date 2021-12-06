fa4d74a6a914d7507f7436d3b7a0afff7e0b623f
superhuman <superhuman@eduroam-193-157-179-45.wlan.uio.no>
2021-12-06 12:43:15 +0100

    s

---
+++ bert_pytorch/trainer/replay_loader.py
@@ -0,0 +1,10 @@
+import pickle
+import gzip
+import numpy as np
+import tensorflow as tf
+fname = '../Freeway/5/replay_logs/$store$_terminal_ckpt.39.gz'
+with tf.io.gfile.GFile(fname, 'rb') as f:
+    with gzip.GzipFile(fileobj=f) as infile:
+        x = np.load(infile)
+
+print(len(x))
