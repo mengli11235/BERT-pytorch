93069d57d126bf96ec5724b0aac4798a2d3d0f56
superhuman <superhuman@eduroam-193-157-179-45.wlan.uio.no>
2021-12-06 12:59:18 +0100

    s

--- bert_pytorch/trainer/replay_loader.py
+++ bert_pytorch/trainer/replay_loader.py
@@ -2,7 +2,7 @@ import pickle
 import gzip
 import numpy as np
 import tensorflow as tf
-fname = '../Freeway/5/replay_logs/$store$_terminal_ckpt.39.gz'
+fname = '../Replays/Freeway/5/replay_logs/$store$_terminal_ckpt.39.gz'
 with tf.io.gfile.GFile(fname, 'rb') as f:
     with gzip.GzipFile(fileobj=f) as infile:
         x = np.load(infile)
